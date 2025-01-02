3ce4b29434c00e227480e651d59ef539fd62a49c
Lucky Aullia Arisdi <117519001+luckyauars@users.noreply.github.com>
2025-01-02 13:30:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Fungsi untuk menghasilkan warna acak
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+// Event listener untuk tombol
+document.getElementById("ubahWarnaBtn").addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomColor();
+});
